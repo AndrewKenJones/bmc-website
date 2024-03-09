7ce37772ef42dc0a7ef3f1baacf94241e87b944f
Hannya_ <24587879+AndrewKenJones@users.noreply.github.com>
2024-03-09 17:26:15 +0000

    updates

--- assets/script.js
+++ assets/script.js
@@ -1,7 +1,13 @@
+// Navbar button
 let menu = document.querySelector('#menu-btn');
 let navbar = document.querySelector('.header .flex .navbar');
-
+// Navbar button toggle
 menu.onclick = () => {
     menu.classList.toggle('fa-times');
     navbar.classList.toggle('active');
+}
+// Navbar status on scroll
+window.onscroll = () => {
+    menu.classList.remove('fa-times');
+    navbar.classList.remove('active');
 }
